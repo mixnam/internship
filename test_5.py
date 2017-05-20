748e16ade951004d13d999a77e540a01bb1730ee
mixnam <mixnam0905@gmail.com>
2017-05-20 11:08:29 +0300

    PEP8 | 5 test

--- test_5.py
+++ test_5.py
@@ -1,5 +1,10 @@
-list_squared = lambda x : [i*i for i in x]
+def list_squared(x):
+    return [i*i for i in x]
 
-list_second_item = lambda x : [x[i] for i in xrange(len(x)) if i%2 != 0]
 
-list_squared_2 = lambda x : [pow(x[i],2) for i in xrange(len(x)) if i%2 == 0 and x[i]%2 == 0 and x[i] != 0]
+def list_second_item(x):
+    return [x[i] for i in xrange(len(x)) if i % 2 != 0]
+
+
+def list_squared_2(x):
+    return [x[i]**2 for i in xrange(1, len(x), 2) if x[i] % 2 == 0]
