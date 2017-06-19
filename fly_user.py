b4f4e27c81464fe05a3141dafdd5bdd356cd9d49
mixnam <mixnam0905@gmail.com>
2017-06-19 16:43:23 +0300

    new commit

--- fly_user.py
+++ fly_user.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 import datetime
 import argparse
-from scraper import Scraper
+import sys
+from scraper import Scraper, ScraperError
+
+
+class ValidationError(Exception):
+    def __init__(self, value):
+        self.value = value
 
 
 def validation_date_str(date_str):
@@ -10,8 +16,8 @@ def validation_date_str(date_str):
         return date
     except ValueError:
         msg = ("\nYou should specify date like this: DD-MM-YYYY " +
-               "\nNot like this : {0}").format(date_str)
-        raise argparse.ArgumentTypeError(msg)
+               "\nNot like this : {0}\n").format(date_str)
+        raise ValidationError(msg)
 
 
 def validation_date(date_high, date_low=0):
@@ -25,55 +31,59 @@ def validation_IATA(IATAcode):
     if IATAcode.isalpha() and IATAcode.isupper() and len(IATAcode) == 3:
         return IATAcode
     else:
-        msg = "\nYou specify wrong IATA-code, it should be like this : 'AAA'"
-        raise argparse.ArgumentTypeError(msg)
+        msg = "\nYou specify wrong IATA-code, it should be like this : 'AAA'\n"
+        raise ValidationError(msg)
 
 
-def validation(outboundDate, returnDate=0):
-    if not returnDate:
-        returnDate = outboundDate
+def check_dates(outbound_date, return_date=0):
+    if not return_date:
+        return_date = outbound_date
 
-    if not validation_date(outboundDate):
-        print ("You can't spesify outbound date erlier than {0}\n" +
-               "Please check that"
+    if not validation_date(outbound_date):
+        msg = ("You can't spesify outbound date erlier than {0}\n" +
+               "Please check that\n"
                ).format(datetime.datetime.now().strftime("%d-%m-%Y"))
-        return False
-    elif not validation_date(returnDate, outboundDate):
-        print "Your return date can't be before outbound date" +\
-                "\nPlease check that"
-        return False
+        raise ValidationError(msg)
+    elif not validation_date(return_date, outbound_date):
+        msg = "Your return date can't be before outbound date" +\
+                "\nPlease check that\n"
+        raise ValidationError(msg)
     else:
         return True
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("departureIATA",
-                        type=validation_IATA,
-                        help="Specify IATA-code of your departure airport")
-    parser.add_argument("destinationIATA",
-                        type=validation_IATA,
-                        help="Specify IATA-code of your destination airport")
-    parser.add_argument("outboundDate",
-                        type=validation_date_str,
-                        help="Specify outbound date")
-    parser.add_argument("returnDate",
-                        nargs="?",
-                        default=0,
-                        type=validation_date_str,
-                        help=("If you don't want fly oneway," +
-                              " specify return date"))
-    args = parser.parse_args()
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("departure_IATA",
+                            type=validation_IATA,
+                            help="Specify IATA-code of your departure airport")
+        parser.add_argument("destination_IATA",
+                            type=validation_IATA,
+                            help="Specify IATA of your destination airport")
+        parser.add_argument("outbound_date",
+                            type=validation_date_str,
+                            help="Specify outbound date")
+        parser.add_argument("return_date",
+                            nargs="?",
+                            default=0,
+                            type=validation_date_str,
+                            help=("If you don't want fly oneway," +
+                                  " specify return date"))
+        args = parser.parse_args()
 
-    if validation(args.outboundDate, args.returnDate):
-        s = Scraper(args.departureIATA,
-                    args.destinationIATA,
-                    args.outboundDate,
-                    args.returnDate)
-        s.print_info()
-    else:
-        print "Something wrong"
+        if check_dates(args.outbound_date, args.return_date):
+            s = Scraper(args.departure_IATA,
+                        args.destination_IATA,
+                        args.outbound_date,
+                        args.return_date)
+            s.print_info()
+
+        return 0
+    except (ScraperError, ValidationError) as err:
+        sys.stderr.write(err.value)
+        return 1
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
--- scraper.py
+++ scraper.py
@@ -1,98 +1,130 @@
 # -*- coding: utf-8 -*-
 import requests
 from lxml import html
+from decimal import Decimal
 
 
-class MyException(Exception):
-    def __init__(self, text):
-        print text
+def try_KeyError(f):
+    def wrap(self, *args):
+        try:
+            return f(self, *args)
+        except KeyError:
+            raise ScraperError("что-то не так в make_results")
+    return wrap
 
 
-class Scraper():
-    def __init__(self, departure, destination, outboundDate, returnDate=0):
+class ScraperError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+
+class Scraper(object):
+    def __init__(self, departure, destination, outbound_date, return_date=0):
         self.departure = departure
         self.destination = destination
-        self.outboundDate = outboundDate.strftime("%Y-%m-%d")
-        if not returnDate:
-            self.oneway_ajax = "on"
-            self.oneway_url = 1
-            self.returnDate = outboundDate.strftime("%Y-%m-%d")
+        self.outbound_date = outbound_date.strftime("%Y-%m-%d")
+        if not return_date:
+            self.oneway_bool_ajax = "on"
+            self.oneway_bool_url = 1
+            self.return_date = outbound_date.strftime("%Y-%m-%d")
         else:
-            self.oneway_ajax = ""
-            self.oneway_url = 0
-            self.returnDate = returnDate.strftime("%Y-%m-%d")
+            self.oneway_bool_ajax = ""
+            self.oneway_bool_url = 0
+            self.return_date = return_date.strftime("%Y-%m-%d")
 
-        self.ajax_dict = {"_ajax[templates][]": ["main",
-                                                 "priceoverview",
-                                                 "infos",
-                                                 "flightinfo"],
-                          "_ajax[requestParams][openDateOverview]": "",
-                          "_ajax[requestParams][departure]": self.departure,
-                          "_ajax[requestParams][childCount]": 0,
-                          "_ajax[requestParams][returnDate]": self.returnDate,
-                          "_ajax[requestParams]" +
-                          "[outboundDate]": self.outboundDate,
-                          "_ajax[requestParams][oneway]": self.oneway_ajax,
-                          "_ajax[requestParams][adultCount]": 1,
-                          "_ajax[requestParams]" +
-                          "[destination]": self.destination,
-                          "_ajax[requestParams][infantCount]": 0,
-                          "_ajax[requestParams][returnDestination]": "",
-                          "_ajax[requestParams][returnDeparture]": ""}
-
-        self.url = ("https://www.flyniki.com/ru/booking/" +
-                    "flight/vacancy.php?departure={0}" +
-                    "&destination={1}&outboundDate={2}&returnDate={3}" +
-                    "&oneway={4}&openDateOverview=0&adultCount=1&" +
-                    "childCount=0&infantCount=0").format(self.departure,
-                                                         self.destination,
-                                                         self.outboundDate,
-                                                         self.returnDate,
-                                                         self.oneway_url)
-        self.json = self.get_json()
-        self.tree = self.make_tree()
-        self.outbound_vacancy = self.get_vacancy("outbound block")
-        self.return_vacancy = self.get_vacancy("return block")
-
-        self.print_results(self.outbound_vacancy)
-        self.print_results(self.return_vacancy)
+        self.make_search()
 
     def print_info(self):
         print "Departure: %s" % self.departure
         print "Destination: %s" % self.destination
-        print "Out bound date: %s" % self.outboundDate
-        print "Return date: %s" % self.returnDate
+        print "Outbound date: %s" % self.outbound_date
+        print "Return date: %s" % self.return_date
 
     def get_json(self):
+        ajax_dict = {"_ajax[templates][]": ["main",
+                                            "priceoverview",
+                                            "infos",
+                                            "flightinfo"],
+                     "_ajax[requestParams][openDateOverview]": "",
+                     "_ajax[requestParams][departure]": self.departure,
+                     "_ajax[requestParams][childCount]": 0,
+                     "_ajax[requestParams][returnDate]": self.return_date,
+                     "_ajax[requestParams]" +
+                     "[outboundDate]": self.outbound_date,
+                     "_ajax[requestParams][oneway]": self.oneway_bool_ajax,
+                     "_ajax[requestParams][adultCount]": 1,
+                     "_ajax[requestParams]" +
+                     "[destination]": self.destination,
+                     "_ajax[requestParams][infantCount]": 0,
+                     "_ajax[requestParams][returnDestination]": "",
+                     "_ajax[requestParams][returnDeparture]": ""}
+
+        url = ("https://www.flyniki.com/ru/booking/" +
+               "flight/vacancy.php?departure={0}" +
+               "&destination={1}&outboundDate={2}&returnDate={3}" +
+               "&oneway={4}&openDateOverview=0&adultCount=1&" +
+               "childCount=0&infantCount=0").format(self.departure,
+                                                    self.destination,
+                                                    self.outbound_date,
+                                                    self.return_date,
+                                                    self.oneway_bool_url)
+
         with requests.Session() as s:
-            request_get = s.get(self.url)
-            request_post = s.post(request_get.url, data=self.ajax_dict)
+            request_get = s.get(url)
+            request_post = s.post(request_get.url, data=ajax_dict)
             if request_post.json():
                 return request_post.json()
             else:
-                raise MyException("No json")
+                raise ScraperError("No json")
 
+    @try_KeyError
     def make_results(self, path):
         tr = path.getchildren()
         count = len(tr)/2
         results = {}
         for i in range(count):
+            currency_comf = self.tree.get_element_by_id("flight-table-" +
+                                                        "header-price-" +
+                                                        "ECO_COMF")
+            currency_prem = self.tree.get_element_by_id("flight-table-" +
+                                                        "header-price-" +
+                                                        "ECO_PREM")
+
             price_comf = path.get_element_by_id("priceLabelIdCOMFFi_" + str(i))
             price_comf = price_comf.xpath(".//div[@class='current']/span")[0]
+            price_comf = (Decimal(price_comf.text.split(",")[0]),
+                          currency_comf)
 
             price_prem = path.get_element_by_id("priceLabelIdPREMFi_" + str(i))
-            price_prem = price_prem.xpath(".//div[@class='current']/span")[0]
+            price_prem = price_prem.xpath(".//div[@class='lowest']/span")[0]
+            price_prem = (Decimal(price_prem.text.split(",")[0]),
+                          currency_prem)
+
+            try:
+                currency_bus = self.tree.get_element_by_id("flight-table-" +
+                                                           "header-price-" +
+                                                           "BUS_FLEX")
+                price_bus = path.get_element_by_id("priceLabelIdFLEXFi_" +
+                                                   str(i))
+                price_bus = price_bus.xpath(".//div[@class='lowest']/span")[0]
+                price_bus = (Decimal(price_bus.text.split(",")[0]),
+                             currency_bus)
+            except KeyError:
+                price_bus = "нет доступных мест"
 
             departure = path.get_element_by_id("flightDepartureFi_" + str(i))
             departure_time = [j.text for j in departure.getchildren()]
             departure_time = "-".join(departure_time)
 
             duration = path.get_element_by_id("flightDurationFi_"+str(i))
+            duration = duration.text
 
-            results[i] = {"price comfort": price_comf.text,
-                          "price premium": price_prem.text,
+            results[i] = {"price comfort": price_comf,
+                          "price premium": price_prem,
+                          "price busines": price_bus,
                           "departure": departure_time,
-                          "duration": duration.text}
+                          "duration": duration}
+
         return results
 
     def make_tree(self):
@@ -105,7 +137,7 @@ class Scraper():
             tree = html.fromstring(main)
             error = tree.xpath("//div[@class='wrapper']/" +
                                "div[@class='entry formCheck']/p")[0]
-            raise MyException(error.text)
+            raise ScraperError(error.text + "\n")
 
     def get_vacancy(self, block):
         try:
@@ -116,14 +148,45 @@ class Scraper():
                                          "/tbody").format(block))[0]
             return self.make_results(tbody)
         except KeyError:
-                raise MyException("Не удалось найти рейсов " +
-                                  "на запрошенные даты")
+            raise ScraperError("Не удалось найти рейсов " +
+                               "на запрошенные даты\n")
         except IndexError:
-                return None
+            return None
+
+    def make_search(self):
+        self.json = self.get_json()
+        self.tree = self.make_tree()
+
+        outbound_vacancy = self.get_vacancy("outbound block")
+        return_vacancy = self.get_vacancy("return block")
+
+        self.print_results(outbound_vacancy)
+        self.print_results(return_vacancy)
+
+    # def sort_results(self, val, val2):
+    #     k = ["EL","EF","BS"]
+    #     k = ["-".join(i) for i in itertools.product(k,k)]
+    #     v = [0]*10
+    #     sorted_results = dict(zip(k,v))
+    #     if type(val) and type(val2) == dict:
+    #         for i in sorted_results.keys():
+    #             for j in val:
+    #                 for k in val2:
+    #                     sorted_results[i] = {j+k: }
 
     def print_results(self, val):
         if type(val) == dict:
             for i in val.keys():
-                print i, val[i]
+                print ("{0} вариант:\nвремя вылета-прилета {1}, " +
+                       "длилетльность перелета - {2}\nцена " +
+                       "'econom light' - {3}" +
+                       "\nцена 'econom flex' - {4}" +
+                       "\nцена 'busines' - " +
+                       "{5}").format(i + 1,
+                                     val[i]["departure"],
+                                     val[i]["duration"],
+                                     str(val[i]["price comfort"][0]),
+                                     str(val[i]["price premium"][0]),
+                                     str(val[i]["price busines"][0]))
         else:
             pass
